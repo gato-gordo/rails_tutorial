a60b673f03c57da867195b3541d8122368b2abb9
Ignacio Prado <ignacio@gatogordoweb.com>
2014-10-09 15:56:21 -0500

    Add Contact Page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,17 +7,46 @@ describe "StaticPages" do
 	 visit '/static_pages/home'
       expect(page).to have_title('Sample App | Home')
     end
+
+    it "should have the content 'Home' " do
+      visit '/static_pages/home'
+	 expect(page).to have_content('Home')
+    end
   end
+
   describe "Help Page" do
     it "should have the right title 'Sample App | Help'" do
       visit '/static_pages/help'
 	 expect(page).to have_title('Sample App | Help')
     end
+
+    it "should have the content 'Help'" do
+    	 visit '/static_pages/help'
+	 expect(page).to have_content('Help')
+    end
+
   end
-  describe "Abouti Page" do
+  describe "About Page" do
     it "should have the right title 'Sample App | About'" do
       visit '/static_pages/about'
 	 expect(page).to have_title('Sample App | About')
+    end
+
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
+	 expect(page).to have_content('About')
+    end
+  end
+
+  describe "Contact Page" do
+    it "should have the right title 'Sample App | Contact'" do
+      visit '/static_pages/contact'
+	 expect(page).to have_title('Contact')
+    end
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+	 expect(page).to have_content('Contact')
     end
   end
 end
